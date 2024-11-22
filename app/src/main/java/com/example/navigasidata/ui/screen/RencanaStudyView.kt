80fd996b2b1260df0b4309c6e81aba39a4b54e46
hi-lufz1 <latifufauzi@gmail.com>
2024-11-22 14:34:13 +0700

    menambah gambar

--- app/src/main/java/com/example/navigasidata/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasidata/ui/screen/RencanaStudyView.kt
@@ -1,16 +1,28 @@
 package com.example.navigasidata.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.navigasidata.model.Mahasiswa
 import com.example.navigasidata.R
 import com.example.navigasidata.model.RencanaStudy
@@ -38,9 +50,22 @@ fun RencanaStudyView(
         modifier = Modifier
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary ))
-    )
-
-    {  }
+    ) {
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row ( modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            Image(
+                painter = painterResource(id = R.drawable.s14),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
+        }
+    }
 
 
 
