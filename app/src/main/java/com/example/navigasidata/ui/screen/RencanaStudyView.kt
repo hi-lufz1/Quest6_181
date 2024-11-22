3b7ae72690f165bad5fd07883a3a9b0c3d9ed10f
hi-lufz1 <latifufauzi@gmail.com>
2024-11-22 14:24:52 +0700

    membuat kolom

--- app/src/main/java/com/example/navigasidata/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigasidata/ui/screen/RencanaStudyView.kt
@@ -1,12 +1,18 @@
 package com.example.navigasidata.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import com.example.navigasidata.model.Mahasiswa
+import com.example.navigasidata.R
 import com.example.navigasidata.model.RencanaStudy
 
 @Composable
@@ -27,6 +33,14 @@ fun RencanaStudyView(
     }
     var listData: MutableList<String> =
         mutableListOf(chosenDropdown, pilihanKelas)
+
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary ))
+    )
+
+    {  }
 
 
 
